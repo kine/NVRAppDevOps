0f7534a5631f23a8540c216dad4db5fde4f6ffae
KSACEK <kamil.sacek@navertica.com>
2018-09-04 10:19:15 +0200

    feat(AppTree): Add uninstall for tree

---
+++ Uninstall-ALAppTree.ps1
@@ -0,0 +1,15 @@
+function Uninstall-ALAppTree
+{
+    Param(
+        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        $ContainerName=$env:ContainerName,
+        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        $AppName=$env:RELEASE_DEFINITIONNAME,
+        $OrderedApps
+
+    )
+
+    for ($i=$OrderedApps.Count;$i -gt 0;$i--) {
+        UnInstall-NavContainerApp -containerName $ContainerName -appName $OrderedApps[$i-1].name
+    }
+}
