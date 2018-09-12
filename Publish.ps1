c5a9567aa1531dc38e1e23b0eb45bfe0fc278a11
KSACEK <kamil.sacek@navertica.com>
2018-09-12 16:22:11 +0200

    chore(Internal): Remove publish file

--- Publish.ps1
+++
@@ -1,8 +0,0 @@
-function PublishNVRAppDevOpsModule
-{
-    UnRegister-PSRepository -Name "NVRTools"
-    $cred = Get-Credential
-    Register-PSRepository -Name "NVRTools" -SourceLocation "https://navertica.pkgs.visualstudio.com/_packaging/NVRTools/nuget/v2" -PublishLocation "https://navertica.pkgs.visualstudio.com/_packaging/NVRTools/nuget/v2" -Credential $cred -InstallationPolicy Trusted -Verbose
-    nuget sources add -Name NVRTools -Source 'https://navertica.pkgs.visualstudio.com/_packaging/NVRTools/nuget/v2' -Username 'xxx@navertica.com' -Password xxx
-    publish-module -Path .\ -Repository NVRTools -NuGetApiKey VSTS -Credential $cred
-}
