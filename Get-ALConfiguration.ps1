4ab2e32477686bc50ec2e881ce0b6e0168b203d9
KSACEK <kamil.sacek@navertica.com>
2018-09-24 11:00:49 +0200

    refact(Config): Get result path if e.g. something\..\something is used

--- Get-ALConfiguration.ps1
+++ Get-ALConfiguration.ps1
@@ -23,26 +23,35 @@ function Get-ALConfiguration
         $AppDownloadScript
     )
 
+    function Get-ResultPath 
+    {
+        param(
+            $Path
+        )
+        $ResultPath = (Get-Item -Path $Path).FullName
+        return $ResultPath
+    }
+
     $Configuration = New-Object -TypeName PSObject
     $Configuration | Add-Member -MemberType NoteProperty -Name 'ContainerName' -Value $ContainerName
     $Configuration | Add-Member -MemberType NoteProperty -Name 'ImageName' -Value $ImageName
     $Configuration | Add-Member -MemberType NoteProperty -Name 'LicenseFile' -Value $LicenseFile
-    $Configuration | Add-Member -MemberType NoteProperty -Name 'VsixPath' -Value $VsixPath
+    $Configuration | Add-Member -MemberType NoteProperty -Name 'VsixPath' -Value (Get-ResultPath -Path $VsixPath)
     $Configuration | Add-Member -MemberType NoteProperty -Name 'PlatformVersion' -Value $PlatformVersion
     $Configuration | Add-Member -MemberType NoteProperty -Name 'AppVersion' -Value $AppVersion
     $Configuration | Add-Member -MemberType NoteProperty -Name 'TestAppVersion' -Value $TestAppVersion
     $Configuration | Add-Member -MemberType NoteProperty -Name 'AppName' -Value $AppName
     $Configuration | Add-Member -MemberType NoteProperty -Name 'TestAppName' -Value $TestAppName
-    $Configuration | Add-Member -MemberType NoteProperty -Name 'AppFile' -Value $AppFile
-    $Configuration | Add-Member -MemberType NoteProperty -Name 'TestAppFile' -Value $TestAppFile
+    $Configuration | Add-Member -MemberType NoteProperty -Name 'AppFile' -Value (Get-ResultPath -Path $AppFile)
+    $Configuration | Add-Member -MemberType NoteProperty -Name 'TestAppFile' -Value (Get-ResultPath -Path $TestAppFile)
     $Configuration | Add-Member -MemberType NoteProperty -Name 'Publisher' -Value $Publisher
     $Configuration | Add-Member -MemberType NoteProperty -Name 'TestPublisher' -Value $TestPublisher
-    $Configuration | Add-Member -MemberType NoteProperty -Name 'RepoPath' -Value $RepoPath
-    $Configuration | Add-Member -MemberType NoteProperty -Name 'AppPath' -Value $AppPath
-    $Configuration | Add-Member -MemberType NoteProperty -Name 'TestAppPath' -Value $TestAppPath
+    $Configuration | Add-Member -MemberType NoteProperty -Name 'RepoPath' -Value (Get-ResultPath -Path $RepoPath)
+    $Configuration | Add-Member -MemberType NoteProperty -Name 'AppPath' -Value (Get-ResultPath -Path $AppPath)
+    $Configuration | Add-Member -MemberType NoteProperty -Name 'TestAppPath' -Value (Get-ResultPath -Path $TestAppPath)
     $Configuration | Add-Member -MemberType NoteProperty -Name 'Build' -Value $Build
     $Configuration | Add-Member -MemberType NoteProperty -Name 'Password' -Value $Password
-    $Configuration | Add-Member -MemberType NoteProperty -Name 'ClientPath' -Value $ClientPath
+    $Configuration | Add-Member -MemberType NoteProperty -Name 'ClientPath' -Value (Get-ResultPath -Path $ClientPath)
     $Configuration | Add-Member -MemberType NoteProperty -Name 'AppDownloadScript' -Value $AppDownloadScript
     
 
