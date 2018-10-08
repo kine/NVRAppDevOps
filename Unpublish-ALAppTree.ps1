fe818be806482578f30e24a452e387e7f57ea737
KSACEK <kamil.sacek@navertica.com>
2018-10-08 16:33:28 +0200

    fix(Unpublish): Unpublish fixed, removed same version detection

--- Unpublish-ALAppTree.ps1
+++ Unpublish-ALAppTree.ps1
@@ -10,12 +10,12 @@ function Unpublish-ALAppTree
         Write-Host "Unpublishing app $($OrderedApps[$i-1].name)"
         $AppName = $OrderedApps[$i-1].name
         $dockerapp = Get-NavContainerAppInfo -containerName $ContainerName | where-object {$_.Name -eq $AppName}
-        $app = $AppFile #Get-ChildItem $env:AGENT_RELEASEDIRECTORY -Recurse -Filter *.app | Select-Object -Last 1
+        $app = $OrderedApps[$i-1].AppPath #Get-ChildItem $env:AGENT_RELEASEDIRECTORY -Recurse -Filter *.app | Select-Object -Last 1
         if ($app) {
-            if ((-not $dockerapp) -or $app.FullName.Contains($dockerapp.Version)) {
-                Write-Host "##vso[task.setvariable variable=SameVersionExists]true"
-                Write-Host "Same version detected!!! ($($dockerapp.Version))"
-            }
+            #if ((-not $dockerapp) -or $app.FullName.Contains($dockerapp.Version)) {
+                #Write-Host "##vso[task.setvariable variable=SameVersionExists]true"
+            #    Write-Host "Same version detected!!! ($($dockerapp.Version))"
+            #}
             if ($dockerapp) {
                 Write-Host "Unpublishing version $($dockerapp.Version)"
                 Unpublish-NavContainerApp -containerName $ContainerName -appName $AppName -uninstall
