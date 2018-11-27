3ab6ba8fe03846fc7437e52fa7d62e20b3ccfe40
KSACEK <kamil.sacek@navertica.com>
2018-11-27 15:50:20 +0100

    fix(Sign): Add sign tool test into Sign-ALApp

--- Sign-ALApp.ps1
+++ Sign-ALApp.ps1
@@ -9,5 +9,11 @@ function Sign-ALApp
         $CertPwd
     ) 
 
-    SignTool sign /f $CertPath /p $CertPwd /t http://timestamp.verisign.com/scripts/timestamp.dll $AppFile
+    if (Test-Path "C:\Program Files (x86)\Windows Kits\10\bin\*\x64\SignTool.exe") {
+        $SignTool = (get-item "C:\Program Files (x86)\Windows Kits\10\bin\*\x64\SignTool.exe").FullName
+    } else {
+        throw "Couldn't find SignTool.exe, please install Windows SDK from https://go.microsoft.com/fwlink/p/?LinkID=2023014"
+    }
+    
+    $SignTool sign /f $CertPath /p $CertPwd /t http://timestamp.verisign.com/scripts/timestamp.dll $AppFile
 }
