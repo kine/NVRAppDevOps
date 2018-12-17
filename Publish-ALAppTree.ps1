b37e30fcc3ea3440a4cbbd3cee15a662962a021c
KSACEK <kamil.sacek@navertica.com>
2018-12-17 16:53:43 +0100

    feat(Publish):Added parameters to set tenant and syncMode

--- Publish-ALAppTree.ps1
+++ Publish-ALAppTree.ps1
@@ -5,7 +5,13 @@ function Publish-ALAppTree
       $ContainerName=$env:ContainerName,
       $SkipVerification=$env:SKIPVERIFICATION,
       $OrderedApps,
-      $PackagesPath
+      $PackagesPath,
+      [ValidateSet('Add','Clean','Development')]
+      [Parameter(ValueFromPipelineByPropertyName=$True)]
+      [string]$syncMode = 'Development',
+      [ValidateSet('Global','Tenant')]
+      [Parameter(ValueFromPipelineByPropertyName=$True)]
+      [string]$scope = 'Tenant' 
   )
   if (-not $PackagesPath) {
     $PackagesPath = Get-Location
@@ -18,9 +24,9 @@ function Publish-ALAppTree
       $AppFile = (Get-ChildItem -Path $PackagesPath -Filter "$($App.publisher)_$($App.name)_*.app" | Select-Object -First 1).FullName
     }
     if ($SkipVerification -eq 'true') {
-      Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile -SkipVerification -sync -install
+      Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile -SkipVerification -sync -install -syncMode $syncMode -scope $scope
     } else {
-      Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile -sync -install
+      Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile -sync -install -syncMode $syncMode -scope $scope
     }
   }
 }
