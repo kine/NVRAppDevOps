108ede2fa9aadd2190e549afe68743d14f2aa5ef
KSACEK <kamil.sacek@navertica.com>
2018-12-17 16:47:43 +0100

    feat(Publish): Added syncMode and scope parameters added into navcontainerhelper

--- Publish-ALApp.ps1
+++ Publish-ALApp.ps1
@@ -5,11 +5,18 @@ function Publish-ALApp
       $ContainerName=$env:ContainerName,
       [Parameter(ValueFromPipelineByPropertyName=$True)]
       $AppFile,
-      $SkipVerification=$env:SKIPVERIFICATION
+      $SkipVerification=$env:SKIPVERIFICATION,
+      [ValidateSet('Add','Clean','Development')]
+      [Parameter(ValueFromPipelineByPropertyName=$True)]
+      [string]$syncMode = 'Development',
+      [ValidateSet('Global','Tenant')]
+      [Parameter(ValueFromPipelineByPropertyName=$True)]
+      [string]$scope = 'Tenant' 
   )
+
   if ($SkipVerification -eq 'true') {
-    Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile -SkipVerification
+    Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile -SkipVerification -syncMode $syncMode -scope $scope
   } else {
-    Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile
+    Publish-NavContainerApp -containerName $ContainerName -appFile $AppFile -syncMode $syncMode -scope $scope
   }
 }
