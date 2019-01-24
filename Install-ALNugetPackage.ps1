254ed0e62b3bd8ada0c5f3a64876bffa1c7b14e6
KSACEK <kamil.sacek@navertica.com>
2019-01-24 08:08:23 +0100

    feat(Nuget): Use default sources when source is not passed

--- Install-ALNugetPackage.ps1
+++ Install-ALNugetPackage.ps1
@@ -23,8 +23,12 @@ function Install-ALNugetPackage
         Remove-Item $TempFolder -Force | Out-Null
     }
     New-Item -Path $TempFolder -ItemType directory -Force | Out-Null
-    Write-Host "Installing package '$IdPrefix$(Format-AppNameForNuget $PackageName)' from '$Source' to $TempFolder..."
-    nuget.exe install -Source "$Source" -Version $Version -OutputDirectory $TempFolder -NoCache "$IdPrefix$(Format-AppNameForNuget $PackageName)"
+    Write-Host "Installing package '$IdPrefix$(Format-AppNameForNuget $PackageName)' from '$Source' to $TargetPath..."
+    if ($Source) {
+        nuget.exe install -Source "$Source" -Version $Version -OutputDirectory $TempFolder -NoCache "$IdPrefix$(Format-AppNameForNuget $PackageName)"
+    } else {
+        nuget.exe install -Version $Version -OutputDirectory $TempFolder -NoCache "$IdPrefix$(Format-AppNameForNuget $PackageName)"
+    }
     Write-Host "Moving app files from $TempFolder to $TargetPath..."
     Get-ChildItem -Path $TempFolder -Filter *.app -Recurse | Copy-Item -Destination $TargetPath -Container -Force | Out-Null
     Write-Host "Removing folder $TempFolder..."
