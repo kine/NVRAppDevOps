e098d9d61faba2d8c93ec7ab8419382c75eb8e9d
Max Belohoubek <max.belohoubek@gob.de>
2019-06-19 11:34:32 +0200

    added ssl "support"

--- Init-ALEnvironment.ps1
+++ Init-ALEnvironment.ps1
@@ -64,8 +64,9 @@ function Init-ALEnvironment
         [Parameter(ValueFromPipelineByPropertyName=$True)]
         $CreateTestWebServices=$true,
         [Parameter(ValueFromPipelineByPropertyName=$True)]
-        $customScripts
-
+        $customScripts,
+        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        $useSSL
     )
     if ($env:TF_BUILD) {
         Write-Host "TF_BUILD set, running under agent, enforcing Build flag"
@@ -100,6 +101,11 @@ function Init-ALEnvironment
         $additionalParameters = @("--volume ""$($RepoPath):C:\app""",
             '-e CustomNavSettings=ServicesUseNTLMAuthentication=true'
         )
+
+        if($useSSL -eq 'true') {
+            $additionalParameters += "--env useSSL=Y"
+        }
+
         if($optionalParameters) {
             $additionalParameters += $optionalParameters
         }
@@ -146,6 +152,14 @@ function Init-ALEnvironment
             '-e CustomNavSettings=ServicesUseNTLMAuthentication=true',
             '-e usessl=N'
         )
+
+        if($useSSL -eq 'true') {
+            $additionalParameters += "--env useSSL=Y"
+        }
+        else {
+            $additionalParameters += "--env useSSL=N"
+        }
+
         if($optionalParameters) {
             $additionalParameters += $optionalParameters
         }
