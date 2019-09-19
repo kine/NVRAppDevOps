021668af1e0687d17db411e1d077ac9e816a403f
Sven Niehus <Sven-Niehus@users.noreply.github.com>
2019-09-19 13:14:46 +0200

    Remove alwaysPull from New-NavContainer

--- Init-ALEnvironment.ps1
+++ Init-ALEnvironment.ps1
@@ -175,7 +175,6 @@ function Init-ALEnvironment
             -enableSymbolLoading:$EnableSymbolLoading `
             -doNotExportObjectsToText `
             -includeCSide:$IncludeCSide `
-            -alwaysPull `
             -includeTestToolkit:$inclTestToolkit `
             -additionalParameters $additionalParameters `
             -memoryLimit $RAM `
