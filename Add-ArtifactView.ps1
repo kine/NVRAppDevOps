721bebbd757ee381cec6789fe3f579f4a33649c5
KSACEK <kamil.sacek@navertica.com>
2020-03-16 08:57:17 +0100

    Add OAuth to add-artifactview

--- Add-ArtifactView.ps1
+++ Add-ArtifactView.ps1
@@ -15,6 +15,8 @@
     Name of the view to add to the package
 .Parameter PAT
     PAT to use when connecting to Azure DevOps
+.Parameter OAuthToken
+    OAuthToken to use for athentication (e.g. System.AccessToken from build agent)
 .EXAMPLE
     PS C:\> Add-ArtifactView -accountName mycompany -feedName 31ea740e-abcd-abcd-abcd-112244334455 -packageName mypackagename -packageVersion 1.0.1234 -packageQuality 'Dev' -PAT $mypat
     Will add view Dev to package mypackagename in given feed and account
@@ -37,7 +39,9 @@ function Add-ArtifactView
     [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName=$True)]
     [string]$PAT,
     [Parameter(ValueFromPipelineByPropertyName=$True)]
-    [string]$packageType='nuget'
+    [string]$packageType='nuget',
+    [Parameter(ValueFromPipelineByPropertyName=$True)]
+    [string]$OAuthToken=''
   )
   
   $ErrorActionPreference = "Stop"
@@ -46,7 +50,13 @@ function Add-ArtifactView
   Write-Verbose -Message $json
   
   $Token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($PAT)"))
-  $HeaderPatch = @{"Authorization" = "Basic "+$Token; "content-type" = "application/json-patch+json"}
+  if (-not $OAuthToken) {
+    Write-Verbose -Message 'PAT authorization used'
+    $HeaderPatch = @{"Authorization" = "Basic "+$Token; "content-type" = "application/json-patch+json"}
+  } else {
+    Write-Verbose -Message 'OAuth authorization used'
+    $HeaderPatch = @{"Authorization" = $OAuthToken; "content-type" = "application/json-patch+json"}
+  }
   $tfsCollectionUri = "https://pkgs.dev.azure.com/$accountName"
 
   $requestUri = $tfsCollectionUri + "/_apis/packaging/feeds/$feedName/$packageType/packages/$packageName/versions/$packageVersion" + "?api-version=5.0-preview.1"
