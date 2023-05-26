cf261b47324a7b874888ad1f82697fd559f01c5b
ksacek <kamil.sacek@navertica.com>
2023-05-26 13:24:43 +0200

    Paket batch downlaod in Compile-ALProjectTree

--- NVRAppDevOps/Compile-ALProjectTree.ps1
+++ NVRAppDevOps/Compile-ALProjectTree.ps1
@@ -38,42 +38,43 @@
     File with rulesets to use during compilation
     
 #>
-function Compile-ALProjectTree
-{
+function Compile-ALProjectTree {
     Param (
-        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName='container')]
+        [Parameter(ValueFromPipelineByPropertyName = $True, ParameterSetName = 'container')]
         $ContainerName,
-        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName='artifact')]
+        [Parameter(ValueFromPipelineByPropertyName = $True, ParameterSetName = 'artifact')]
         $ArtifactUrl,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
         $CertPath,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
         $CertPwd,
         $OrderedApps,
         $PackagesPath,
-        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName='container')]
-        $Password='',
-        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName='container')]
-        $Username=$env:USERNAME,
+        [Parameter(ValueFromPipelineByPropertyName = $True, ParameterSetName = 'container')]
+        $Password = '',
+        [Parameter(ValueFromPipelineByPropertyName = $True, ParameterSetName = 'container')]
+        $Username = $env:USERNAME,
         [ValidateSet('Windows', 'NavUserPassword')]
-        [Parameter(ValueFromPipelineByPropertyName=$True,ParameterSetName='container')]
-        $Auth='Windows',
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        [bool]$EnableCodeCop=$True,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        [bool]$EnableAppSourceCop=$True,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        [bool]$EnablePerTenantExtensionCop=$True,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        [bool]$EnableUICop=$True,
-        [ValidateSet('none','error','warning')]
+        [Parameter(ValueFromPipelineByPropertyName = $True, ParameterSetName = 'container')]
+        $Auth = 'Windows',
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        [bool]$EnableCodeCop = $True,
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        [bool]$EnableAppSourceCop = $True,
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        [bool]$EnablePerTenantExtensionCop = $True,
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        [bool]$EnableUICop = $True,
+        [ValidateSet('none', 'error', 'warning')]
         [string]$FailOn = 'error',
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
         $AppDownloadScript,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
         $RulesetFile,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        $AsmProbingPaths
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        $AsmProbingPaths,
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        [bool]$UsePaket = $false
 
 
     )
@@ -83,7 +84,11 @@ function Compile-ALProjectTree
     if ($artifactPath) {
         $alcPath = Get-ALCompilerFromArtifact -artifactUrl $artifactUrl -TargetPath (Join-Path $env:TEMP 'alc')
     }
-
+    if ($UsePaket) {
+        $dependencies = $OrderedApps | where-object { (-not $_.AppPath) -and ($_.publisher -ne 'Microsoft') }
+        Write-Host "Downloading $($dependencies.count) dependencies from nuget with Paket..."
+        Download-ALApp -dependencies $dependencies -targetPath $PackagesPath -AppDownloadScript $AppDownloadScript
+    }
     foreach ($App in $OrderedApps) {
         if ($App.AppPath) {
             Write-Host "**** Compiling $($App.name) ****"
@@ -92,11 +97,13 @@ function Compile-ALProjectTree
             if ($RulesetFile) {
                 if (-not [System.IO.Path]::IsPathRooted($RulesetFile)) {
                     $NewRulesetFile = (Join-Path $AppPath $RulesetFile)
-                } else {
+                }
+                else {
                     $NewRulesetFile = $RulesetFile
                 }
                 Write-Host "Using ruleset path $RulesetFile $NewRulesetFile ($([System.IO.Path]::IsPathRooted($RulesetFile)))"
-            } else {
+            }
+            else {
 
             }
             if ($ArtifactUrl) {
@@ -117,20 +124,24 @@ function Compile-ALProjectTree
                     -rulesetFile $NewRulesetFile `
                     -assemblyProbingPaths $AsmProbingPaths `
                     -alcPath $alcPath | Out-Null
-            } else {
+            }
+            else {
                 if ($Auth -eq 'NavUserPassword') {
                     $PWord = ConvertTo-SecureString -String $Password -AsPlainText -Force
                     $User = $Username
-                    $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$PWord
+                    $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
                     if ($env:TF_BUILD) {
-                        Compile-AppInBcContainer -containerName $ContainerName -appProjectFolder $AppPath -appOutputFolder $PackagesPath -appSymbolsFolder $PackagesPath -AzureDevOps -credential $credentials -EnableCodeCop:$EnableCodeCop -EnableAppSourceCop:$EnableAppSourceCop -EnablePerTenantExtensionCop:$EnablePerTenantExtensionCop -EnableUICop:$EnableUICop -FailOn $FailOn  -rulesetFile $NewRulesetFile -assemblyProbingPaths $AsmProbingPaths| Out-Null
-                    } else {
-                        Compile-AppInBcContainer -containerName $ContainerName -appProjectFolder $AppPath -appOutputFolder $PackagesPath -appSymbolsFolder $PackagesPath  -credential $credentials -EnableCodeCop:$EnableCodeCop -EnableAppSourceCop:$EnableAppSourceCop -EnablePerTenantExtensionCop:$EnablePerTenantExtensionCop -EnableUICop:$EnableUICop -FailOn $FailOn  -rulesetFile $NewRulesetFile -assemblyProbingPaths $AsmProbingPaths| Out-Null
+                        Compile-AppInBcContainer -containerName $ContainerName -appProjectFolder $AppPath -appOutputFolder $PackagesPath -appSymbolsFolder $PackagesPath -AzureDevOps -credential $credentials -EnableCodeCop:$EnableCodeCop -EnableAppSourceCop:$EnableAppSourceCop -EnablePerTenantExtensionCop:$EnablePerTenantExtensionCop -EnableUICop:$EnableUICop -FailOn $FailOn  -rulesetFile $NewRulesetFile -assemblyProbingPaths $AsmProbingPaths | Out-Null
                     }
-                } else {
+                    else {
+                        Compile-AppInBcContainer -containerName $ContainerName -appProjectFolder $AppPath -appOutputFolder $PackagesPath -appSymbolsFolder $PackagesPath  -credential $credentials -EnableCodeCop:$EnableCodeCop -EnableAppSourceCop:$EnableAppSourceCop -EnablePerTenantExtensionCop:$EnablePerTenantExtensionCop -EnableUICop:$EnableUICop -FailOn $FailOn  -rulesetFile $NewRulesetFile -assemblyProbingPaths $AsmProbingPaths | Out-Null
+                    }
+                }
+                else {
                     if ($env:TF_BUILD) {
                         Compile-AppInBcContainer -containerName $ContainerName -appProjectFolder $AppPath -appOutputFolder $PackagesPath -appSymbolsFolder $PackagesPath -AzureDevOps  -EnableCodeCop:$EnableCodeCop -EnableAppSourceCop:$EnableAppSourceCop -EnablePerTenantExtensionCop:$EnablePerTenantExtensionCop -EnableUICop:$EnableUICop -FailOn $FailOn -rulesetFile $NewRulesetFile -assemblyProbingPaths $AsmProbingPaths | Out-Null
-                    } else {
+                    }
+                    else {
                         Compile-AppInBcContainer -containerName $ContainerName -appProjectFolder $AppPath -appOutputFolder $PackagesPath -appSymbolsFolder $PackagesPath -EnableCodeCop:$EnableCodeCop -EnableAppSourceCop:$EnableAppSourceCop -EnablePerTenantExtensionCop:$EnablePerTenantExtensionCop -EnableUICop:$EnableUICop -FailOn $FailOn  -rulesetFile $NewRulesetFile -assemblyProbingPaths $AsmProbingPaths | Out-Null
                     }
                 }
@@ -141,19 +152,25 @@ function Compile-ALProjectTree
                     Write-Host "Signing the app with $CertPath and password inside container..."
                     #& $SignTool sign /f $CertPath /p $CertPwd /t http://timestamp.verisign.com/scripts/timestamp.dll $AppFileName
                     Sign-BCContainerApp -containerName $ContainerName -appFile $AppFileName -pfxFile $CertPath -pfxPassword (ConvertTo-SecureString -String $CertPwd -AsPlainText -Force)
-                } else {
+                }
+                else {
                     if (Test-Path "C:\Program Files (x86)\Windows Kits\10\bin\*\x64\SignTool.exe") {
                         $SignTool = (get-item "C:\Program Files (x86)\Windows Kits\10\bin\*\x64\SignTool.exe").FullName
-                    } else {
+                    }
+                    else {
                         throw "Couldn't find SignTool.exe, please install Windows SDK from https://go.microsoft.com/fwlink/p/?LinkID=2023014"
                     }
                     Write-Host "Signing the app with $CertPath without password (account permissions inside certificate used)..."
                     & $SignTool sign /f $CertPath /t http://timestamp.verisign.com/scripts/timestamp.dll $AppFileName
                 }
             }
-        } else { #App not found, download
-            if ($App.publisher -ne 'Microsoft') {
-                Download-ALApp -name $App.name -publisher $App.publisher -version $App.version -targetPath $PackagesPath -AppDownloadScript $AppDownloadScript
+        }
+        else {
+            if (-not $UsePaket) {
+                #App not found, download
+                if ($App.publisher -ne 'Microsoft') {
+                    Download-ALApp -name $App.name -publisher $App.publisher -version $App.version -targetPath $PackagesPath -AppDownloadScript $AppDownloadScript
+                }
             }
         }
     }
