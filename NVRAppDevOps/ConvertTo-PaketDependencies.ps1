c9e7b93bcad24a628a589795a278ab9d6cf3e0d9
ksacek <kamil.sacek@navertica.com>
2024-01-30 17:02:44 +0100

    ConvertTo-PaketDependencies and Format-AppNameForNuget

---
+++ NVRAppDevOps/ConvertTo-PaketDependencies.ps1
@@ -0,0 +1,96 @@
+function ConvertTo-PaketDependencies {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$ProjectPath,
+        [string[]]$NuGetSources = @(),
+        [ValidateSet('Max', 'Min')]
+        [string]$Policy = 'Min',
+        [string]$Localization,
+        [switch]$Symbols,
+        [version]$MaxApplicationVersion,
+        [version]$MaxPlatformVersion
+    )
+    if ($Localization -ieq 'W1') {
+        $Localization = ''
+    }
+    $AppJsonPath = Join-Path $ProjectPath "app.json"
+    if (-not (Test-Path $AppJsonPath)) {
+        Write-Error "app.json not found in $ProjectPath"
+        return
+    }
+
+    $PaketDependenciesFilePath = Join-Path $ProjectPath "paket.dependencies"
+
+    if (Test-Path $PaketDependenciesFilePath) {
+        $OldContent = Get-Content $PaketDependenciesFilePath
+    }
+
+    if ((-not $NuGetSources) -and ($OldContent)) {
+        Write-Verbose "No NuGet sources specified, using sources from existing paket.dependencies file"
+        $NuGetSources = $OldContent | Where-Object { $_ -match '^source\s' } | ForEach-Object { $_ -replace '^source\s', '' }
+    }
+
+    $AppJson = Get-Content $AppJsonPath | ConvertFrom-Json
+    $AppJsonDependencies = $AppJson.dependencies
+
+    $PaketDependencies = New-Object System.Collections.ArrayList
+
+    if ($Policy -eq 'Max') {
+        $PaketDependencies.Add("strategy: max           # use the maximum version of transitive dependencies") | out-null
+        $PaketDependencies.Add("lowest_matching: false  # use the highest matching version of a direct dependency") | out-null
+        $PackageSuffix = "`tstrategy:min, lowest_matching:false"
+    }
+    else {
+        $PaketDependencies.Add("strategy: min           # use the minimum version of transitive dependencies") | out-null
+        $PaketDependencies.Add("lowest_matching: true   # use the lowest matching version of a direct dependency") | out-null
+        $PackageSuffix = "`tstrategy:min, lowest_matching:true"
+    }
+    $PaketDependencies.Add("") | out-null
+
+    foreach ($Source in $NuGetSources) {
+        $PaketDependencies.Add("source $Source") | out-null
+    }
+    $PaketDependencies.Add("") | out-null
+    $tag = $Localization
+    if ($Symbols) {
+        $tag += '.Symbols'
+    }
+    foreach ($Dependency in $AppJsonDependencies) {
+        $DependencyName = Format-AppNameForNuget -appname $Dependency.name -publisher $Dependency.publisher -version $Dependency.version -id $Dependency.id -tag $tag
+        $PaketDependencies.Add("nuget $($DependencyName) >= $($Dependency.version)") | out-null
+    }
+    if ($AppJson.application) {
+        $AppDependency = @{
+            appId     = ''
+            name      = "Application"
+            publisher = 'Microsoft'
+            version   = $AppJson.application
+        }
+        $DependencyName = Format-AppNameForNuget -appname $AppDependency.name -publisher $AppDependency.publisher -version $AppDependency.version -id $AppDependency.appId -tag $tag
+        $MaxLimit = ''
+        if ($MaxApplicationVersion) {
+            $MaxLimit = '~> ' + $MaxApplicationVersion
+        }
+        $PaketDependencies.Add("nuget $($DependencyName) $($MaxLimit) >= $($AppDependency.version) $($PackageSuffix)") | out-null
+    }
+    if ($AppJson.platform) {
+        $AppDependency = @{
+            appId     = ''
+            name      = "Platform"
+            publisher = 'Microsoft'
+            version   = $AppJson.platform
+        }
+        $tag = ''
+        if ($Symbols) {
+            $tag = 'Symbols'
+        }
+        $DependencyName = Format-AppNameForNuget -appname $AppDependency.name -publisher $AppDependency.publisher -version $AppDependency.version -id $AppDependency.appId -tag $tag
+        $MaxLimit = ''
+        if ($MaxPlatformVersion) {
+            $MaxLimit = '~> ' + $MaxPlatformVersion
+        }
+        $PaketDependencies.Add("nuget $($DependencyName) $($MaxLimit) >= $($AppDependency.version) $($PackageSuffix)") | out-null
+    }
+
+    $PaketDependencies | Out-File $PaketDependenciesFilePath -Encoding utf8
+}
--- NVRAppDevOps/Format-AppNameForNuget.ps1
+++ NVRAppDevOps/Format-AppNameForNuget.ps1
@@ -1,9 +1,83 @@
-function Format-AppNameForNuget
-{
+function Format-AppNameForNuget {
     Param(
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        $Name
+        [Parameter(Mandatory, ParameterSetName = 'OnlyName', Position = 0)]
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        $Name,
+        [Parameter(Mandatory, ParameterSetName = 'UnifiedNaming')]
+        $appname,
+        [Parameter(Mandatory, ParameterSetName = 'UnifiedNaming')]
+        $publisher,
+        [Parameter(ParameterSetName = 'UnifiedNaming')]
+        $id,
+        [Parameter(ParameterSetName = 'UnifiedNaming')]
+        $tag,
+        [Parameter(ParameterSetName = 'UnifiedNaming')]
+        $version
     )
+    #Taken from bccontainerhelper PR until it is available in the bccontainerhelper
+    function Get-BcNuGetPackageIdTemp {
+        Param(
+            [Parameter(Mandatory = $false)]
+            [string] $packageIdTemplate = '{publisher}.{name}.{tag}.{id}',
+            [Parameter(Mandatory = $true)]
+            [string] $publisher,
+            [Parameter(Mandatory = $true)]
+            [string] $name,
+            [Parameter(Mandatory = $false)]
+            [string] $id = '',
+            [Parameter(Mandatory = $false)]
+            [string] $tag = '',
+            [Parameter(Mandatory = $false)]
+            [string] $version = ''
+        )
+    
+        if ($id) {
+            try { $id = ([GUID]::Parse($id)).Guid } catch { throw "App id must be a valid GUID: $id" }
+        }
+        $nname = $name -replace '[^a-zA-Z0-9_\-]', ''
+        $npublisher = $publisher -replace '[^a-zA-Z0-9_\-]', ''
+        if ($nname -eq '') { throw "App name is invalid: '$name'" }
+        if ($npublisher -eq '') { throw "App publisher is invalid: '$publisher'" }
+    
+        $packageIdTemplate = $packageIdTemplate.replace('{id}', $id).replace('{publisher}', $npublisher).replace('{tag}', $tag).replace('{version}', $version).replace('..', '.').TrimEnd('.')
+        # Max. Length of NuGet Package Id is 100 - we shorten the name part of the id if it is too long
+        $packageId = $packageIdTemplate.replace('{name}', $nname)
+        if ($packageId.Length -ge 100) {
+            if ($nname.Length -gt ($packageId.Length - 99)) {
+                $nname = $nname.Substring(0, $nname.Length - ($packageId.Length - 99))
+            }
+            else {
+                throw "Package id is too long: $packageId, unable to shorten it"
+            }
+            $packageId = $packageIdTemplate.replace('{name}', $nname)
+        }
+        return $packageId
+    }
+    if ($tag -ieq 'W1') {
+        $tag = ''
+    }
 
-    return $Name -replace '\s','_' -replace '&','_' -replace '\(','_' -replace '\)','_' -replace '/','_'
+    if ($appname) {
+        if ($appname -eq 'Platform') {
+            $id = ''
+            $tag = ''
+        }
+        if ($appname -eq 'Application') {
+            $id = ''
+        }
+        $NuGetId = ''
+        try {
+            Write-Verbose "Calling Get-BcNuGetPackageId from bccontainerhelper if exists"
+            $NuGetId = Get-BcNuGetPackageId -publisher $publisher -name $appname -id $id -tag $tag -version $version
+        }
+        catch {
+            Write-Verbose "Calling Get-BcNuGetPackageIdTemp instead"
+            $NuGetId = Get-BcNuGetPackageIdTemp -publisher $publisher -name $appname -id $id -tag $tag -version $version
+        }
+    }
+    else {
+        $NuGetId = $Name -replace '\s', '_' -replace '&', '_' -replace '\(', '_' -replace '\)', '_' -replace '/', '_'
+    }
+    Write-Verbose "NuGetId: $NuGetId"
+    return $NuGetId
 }
