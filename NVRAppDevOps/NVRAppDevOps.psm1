43376b5b4556b94d94ecba6e783113656312887c
ksacek <kamil.sacek@navertica.com>
2024-03-08 23:14:52 +0100

    Write version of the module to output

--- NVRAppDevOps/NVRAppDevOps.psm1
+++ NVRAppDevOps/NVRAppDevOps.psm1
@@ -1,3 +1,7 @@
+$Psd = get-content -Path (Join-Path $PSScriptRoot 'NVRAppDevOps.psd1') -Raw
+$ModuleVersion = (invoke-expression $Psd).ModuleVersion
+$ModulePrerelease = (invoke-expression $Psd).PrivateData.PSData.Prerelease
+Write-Host "NVRAppDevOps version $ModuleVersion $ModulePrerelease"
 Write-Verbose  "Reading scripts from $PSScriptRoot"
 Get-Item $PSScriptRoot  | Get-ChildItem -Recurse -file -Filter '*.ps1' |  Sort Name | foreach {
     Write-Verbose "Loading $($_.Name)"  
