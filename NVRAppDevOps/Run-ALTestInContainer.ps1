b7b78b7af6e574be73506c37fdbab36f7f1a4423
ksacek <kamil.sacek@navertica.com>
2025-07-21 15:38:35 +0200

    Add retry for runing tests

--- NVRAppDevOps/Run-ALTestInContainer.ps1
+++ NVRAppDevOps/Run-ALTestInContainer.ps1
@@ -20,36 +20,35 @@
  .Example
   Run-ALTestInContainer -ContatinerName test
 #>
-function Run-ALTestInContainer
-{
+function Run-ALTestInContainer {
     param(
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
-        $ContainerName=$env:ContainerName,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        $Username=$env:USERNAME,
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        $Password='',
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
+        $ContainerName = $env:ContainerName,
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        $Username = $env:USERNAME,
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        $Password = '',
         [ValidateSet('Windows', 'NavUserPassword')]
-        [Parameter(ValueFromPipelineByPropertyName=$True)]
-        $Auth='Windows',
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
+        [Parameter(ValueFromPipelineByPropertyName = $True)]
+        $Auth = 'Windows',
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
         [string]$tenant = "default",
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
         [string] $testSuite = "DEFAULT",
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
         [string] $XUnitResultFileName,
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
-        [ValidateSet('no','error','warning')]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
+        [ValidateSet('no', 'error', 'warning')]
         [string] $AzureDevOps = 'no',
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
         [switch]$detailed,
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
         [switch]$restartContainerAndRetry,
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
-        [string]$extensionId='',
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
-        [string]$companyName='',
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$True)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
+        [string]$extensionId = '',
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
+        [string]$companyName = '',
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True)]
         [switch]$returnTrueIfAllPassed
     )
     if ($env:TF_BUILD) {
@@ -62,38 +61,57 @@ function Run-ALTestInContainer
             Write-Host "Using passed password"
             $PWord = ConvertTo-SecureString -String $Password -AsPlainText -Force
             $User = $Username
-            $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$PWord
-        } else {
+            $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+        }
+        else {
             if ($Auth -eq 'Windows') {
                 $credentials = $null
-            } else {
+            }
+            else {
                 $credentials = Get-Credential -Message "Enter password you want to use" -UserName $Username
             }
         }
-    } else {
+    }
+    else {
         if ((-not $Password) -or ($Password -eq '')) {
             Write-Host 'Using fixed password and NavUserPassword authentication'
             $PWord = ConvertTo-SecureString -String 'Pass@word1' -AsPlainText -Force
-        } else {
+        }
+        else {
             Write-Host "Using passed password and $Auth authentication"
             $PWord = ConvertTo-SecureString -String $Password -AsPlainText -Force
         }
         $User = $Username
-        $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$PWord
+        $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
     }
 
     Write-Host "Running tests in container through bccontainerhelper..."
-    Run-TestsInBcContainer `
-        -containerName $ContainerName `
-        -tenant $tenant `
-        -credential $credentials `
-        -testSuite $testSuite `
-        -XUnitResultFileName $XUnitResultFileName `
-        -AzureDevOps $AzureDevOps `
-        -detailed:$detailed `
-        -restartContainerAndRetry:$restartContainerAndRetry `
-        -extensionId $extensionId `
-        -companyName $companyName `
-        -returnTrueIfAllPassed:$returnTrueIfAllPassed
+    $Retry = $true
+    while ($true) {
+        try {
+            Run-TestsInBcContainer `
+                -containerName $ContainerName `
+                -tenant $tenant `
+                -credential $credentials `
+                -testSuite $testSuite `
+                -XUnitResultFileName $XUnitResultFileName `
+                -AzureDevOps $AzureDevOps `
+                -detailed:$detailed `
+                -restartContainerAndRetry:$restartContainerAndRetry `
+                -extensionId $extensionId `
+                -companyName $companyName `
+                -returnTrueIfAllPassed:$returnTrueIfAllPassed
+            break
+        }
+        catch {
+            if (($_.Exception.Message -like "*Connecting to remote server*") -and ($Retry -eq $true)) {
+                Write-Host "Retrying to run the test, because last connection to the host failed. This can happen if the script tries restarting the container and the container is not yet ready for WinRM connection after 5 second."
+                $Retry = $false
+            }
+            else {
+                throw $_.Exception.Message
+            }
+        }
+    }
         
 }
